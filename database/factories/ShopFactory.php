26c20bdbe060ada6ce591711c47ad3978d78c86a
Shiiya0418 <52955154+Shiiya0418@users.noreply.github.com>
2023-08-22 17:06:12 +0900

    モデル、シーダーなどを追加

---
+++ database/factories/ShopFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shop>
+ */
+class ShopFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'shop_name' => $this->faker->realText(15),
+            'shop_address' => $this->faker->realText(80)
+        ];
+    }
+}
